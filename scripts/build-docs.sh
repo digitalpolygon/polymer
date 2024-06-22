4645cb455948a64dfa551d8b7e852b0281148317
Les Peabody <lpeabody@gmail.com>
2024-06-22 16:16:43 -0400

    Add last updated date to docs pages.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -9,9 +9,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get the project root directory.
 PROJECT_ROOT="$(dirname "$DIR")"
 
+docker build -f Dockerfile.docs -t digitalpolygon/polymer-docs $PROJECT_ROOT
+
 docker run \
     --rm \
     -p 8000 \
     --user=`id -u`:`id -g` \
-    -v ${PROJECT_ROOT}:/docs squidfunk/mkdocs-material:9.5.26 \
+    -v ${PROJECT_ROOT}:/docs digitalpolygon/polymer-docs \
     "$@"
