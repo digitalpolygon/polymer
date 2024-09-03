d3aa744186cfeaa1574ef060af98b0f6c12adff7
DDEV User <nobody@example.com>
2024-09-03 01:24:55 +0000

    Allow tagging.

--- src/Robo/Commands/Artifact/DeployCommand.php
+++ src/Robo/Commands/Artifact/DeployCommand.php
@@ -20,14 +20,14 @@ use Consolidation\AnnotatedCommand\Attributes\Usage;
 class DeployCommand extends TaskBase
 {
     protected string $excludeFileTemp;
-    protected mixed $deployDir;
-    protected mixed $deployDocroot;
+    protected string $deployDir;
+    protected string $deployDocroot;
     protected mixed $tagSource;
     protected string $branchName;
-    /**
-     * @var mixed|string
-     */
-    protected mixed $commitMessage;
+    protected string $commitMessage;
+    protected ConsoleApplication $application;
+    protected bool $createTag = false;
+    protected string $tagName;
 
     /**
      * This hook will fire for all commands in this command file.
@@ -37,19 +37,29 @@ class DeployCommand extends TaskBase
     #[Hook(type: 'init')]
     public function initialize(): void
     {
+        $this->createTag = false;
         $this->excludeFileTemp = $this->getConfigValue('deploy.exclude_file') . '.tmp';
-        $this->deployDir = $this->getConfigValue('deploy.dir');
-        $this->deployDocroot = $this->getConfigValue('deploy.docroot');
+        if (is_string($this->getConfigValue('deploy.dir'))) {
+            $this->deployDir = $this->getConfigValue('deploy.dir');
+        }
+        if (is_string($this->getConfigValue('deploy.docroot'))) {
+            $this->deployDocroot = $this->getConfigValue('deploy.docroot');
+        }
         if (!$this->deployDir || !$this->deployDocroot) {
             throw new PolymerException('Configuration deploy.dir and deploy.docroot must be set to run this command');
         }
         $this->tagSource = $this->getConfigValue('deploy.tag_source', true);
+        if ($this->getContainer()->get('application') instanceof ConsoleApplication) {
+            $this->application = $this->getContainer()->get('application');
+        } else {
+            throw new PolymerException('Failed to get application instance.');
+        }
     }
 
     /**
      * Builds separate artifact and pushes to 'git.remotes' defined polymer.yml.
      *
-     * @param array<string, int|false|string> $options
+     * @param array<string, bool|string|null|int> $options
      *   The artifact deploy command options.
      *
      * @throws \Robo\Exception\TaskException|\Robo\Exception\AbortTasksException
@@ -63,43 +73,163 @@ class DeployCommand extends TaskBase
     #[Option(name: 'dry-run', description: 'Show the deploy operations without pushing the artifact.')]
     public function deployArtifact(ConsoleIO $io, string $artifact, array $options = ['branch' => InputOption::VALUE_REQUIRED, 'tag' => InputOption::VALUE_REQUIRED, 'commit-msg' => InputOption::VALUE_REQUIRED, 'dry-run' => false]): void
     {
+        if (!$options['tag'] && !$options['branch']) {
+            $this->createTag = (bool) $this->confirm("Would you like to create a tag?", $this->createTag);
+        }
+        $this->commitMessage = $this->getCommitMessage($options);
+
+
+        if ($options['tag'] || $this->createTag) {
+            // Warn if they're creating a tag and we won't tag the source for them.
+            if (!$this->tagSource) {
+                $this->say("Config option deploy.tag_source if FALSE. The source repo will not be tagged.");
+            }
+            $this->deployTag($artifact, $options, $io);
+        } else {
+            $this->deployBranch($artifact, $options, $io);
+        }
+    }
+
+    /**
+     * @param string $artifact
+     * @param array<string, bool|string|null|int> $options
+     * @param ConsoleIO $io
+     * @return void
+     * @throws PolymerException
+     * @throws \Robo\Exception\TaskException
+     */
+    protected function deployBranch(string $artifact, array $options, ConsoleIO $io): void
+    {
         $this->branchName = $this->getBranchName($options);
         $this->prepareDir();
         $this->addGitRemotes();
         $this->checkoutLocalDeployBranch();
         $this->mergeUpstreamChanges();
-        /** @var ConsoleApplication $application */
-        $application = $this->getContainer()->get('application');
-        $this->commitMessage = $this->getCommitMessage($options);
 
         $this->say("Deploying artifact '{$artifact}'...");
 
-        $builder = $this->collectionBuilder($io);
-        $builder
-            ->taskToggleableSymfonyCommand($application->find('artifact:compile'))
+        $this->taskToggleableSymfonyCommand($this->application->find('artifact:compile'))
                 ->arg('artifact', $artifact)
-            ->taskGitStack()
-                ->dir($this->deployDir)
-                ->exec("git rm -r --cached --ignore-unmatch --quiet .")
-                ->add('-A')
-                ->commit($this->commitMessage, '--quiet')
-                ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-            ->taskExecStack()
-                ->dir($this->deployDir);
+                ->run();
+
+        $this->commit();
+
+        $this->push($this->branchName, $options);
+    }
+
+    /**
+     * @param string $artifact
+     * @param array<string, bool|string|null|int> $options
+     *   The artifact deploy command options.
+     * @param ConsoleIO $io
+     * @return void
+     * @throws PolymerException
+     * @throws \Robo\Exception\TaskException
+     */
+    protected function deployTag(string $artifact, array $options, ConsoleIO $io): void
+    {
+        $this->tagName = $this->getTagName($options);
+
+        // If we are building a tag, then we assume that we will NOT be pushing the
+        // build branch from which the tag is created. However, we must still have a
+        // local branch from which to cut the tag, so we create a temporary one.
+        $this->branchName = $this->getDefaultBranchName() . '-temp';
+        $this->prepareDir();
+        $this->addGitRemotes();
+        $this->checkoutLocalDeployBranch();
+
+        $this->taskToggleableSymfonyCommand($this->application->find('artifact:compile'))
+            ->arg('artifact', $artifact)
+            ->run();
+
+        $this->commit();
+        $this->cutTag('build');
+
+        // Check the deploy.tag_source config value and also tag the source repo if
+        // it is set to TRUE (the default).
+        if ($this->tagSource) {
+            $this->cutTag('source');
+        }
+
+        $this->push($this->tagName, $options);
+    }
+
+    /**
+     * Creates a tag on the build repository.
+     *
+     * @param string $repo
+     *   The repo in which a tag should be cut.
+     */
+    protected function cutTag($repo = 'build'): void
+    {
+        $taskGit = $this->taskGitStack()
+            ->tag($this->tagName, $this->commitMessage)
+            ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE);
+
+        if ($repo == 'build') {
+            $taskGit->dir($this->deployDir);
+        }
+
+        $result = $taskGit->run();
+        if (!$result->wasSuccessful()) {
+            throw new PolymerException("Failed to create Git tag!");
+        }
+        $this->say("The tag {$this->tagName} was created on the {$repo} repository.");
+    }
+
+    /**
+     * @param string $ref
+     * @param array<string, bool|string|null|int> $options
+     *   The artifact deploy command options.
+     * @return void
+     * @throws PolymerException
+     * @throws \Robo\Exception\TaskException
+     */
+    protected function push(string $ref, array $options): void
+    {
+        if ($options['dry-run']) {
+            $this->logger?->warning("Skipping push of deployment artifact. deploy.dryRun is set to true.");
+            return;
+        } else {
+            $this->say("Pushing artifact to git.remotes...");
+        }
+
+        $pushTask = $this->taskExecStack()
+            ->dir($this->deployDir);
         /** @var array<int|string,string> $remotes */
         $remotes = $this->getConfigValue('git.remotes');
         foreach ($remotes as $remote) {
             $remote_name = md5($remote);
-            $builder->exec("git push $remote_name $this->branchName");
+            $pushTask->exec("git push $remote_name $ref");
         }
+        $result = $pushTask->run();
 
-        $result = $builder->run();
+        if (!$result->wasSuccessful()) {
+            throw new PolymerException('Failed to push deployment artifact!');
+        }
     }
+
+    protected function commit(): void
+    {
+        $this->say("Committing artifact to <comment>{$this->branchName}</comment>...");
+
+        $result = $this->taskGitStack()
+            ->dir($this->deployDir)
+            ->exec("git rm -r --cached --ignore-unmatch --quiet .")
+            ->add('-A')
+            ->commit($this->commitMessage, '--quiet')
+            ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+            ->run();
+        if (!$result->wasSuccessful()) {
+            throw new PolymerException("Failed to commit deployment artifact!");
+        }
+    }
+
 
     /**
      * Gets the branch name for the deployment artifact.
      *
-     * @param array<string, int|string|false> $options
+     * @param array<string, bool|string|null|int> $options
      *   CLI options for command.
      *
      * @return string
@@ -161,9 +291,7 @@ class DeployCommand extends TaskBase
                 throw new PolymerException('Failed to initialize git repo');
             }
             $this->say("Global .gitignore file is being disabled for this repository to prevent unexpected behavior.");
-            return;
         }
-        throw new PolymerException('Deploy directory is not valid.');
     }
 
     /**
@@ -275,13 +403,13 @@ class DeployCommand extends TaskBase
      *
      * Defaults to the last commit message on the source branch.
      *
-     * @param array<string,int|string|false> $options
+     * @param array<string, bool|string|null|int> $options
      *   CLI options for command.
      *
-     * @return mixed
+     * @return string
      *   The commit message.
      */
-    protected function getCommitMessage(array $options)
+    protected function getCommitMessage(array $options): string
     {
         if (!$options['commit-msg']) {
             $gitLastCommitMessage = '';
@@ -297,9 +425,39 @@ class DeployCommand extends TaskBase
 
 
             return $this->askDefault('Enter a valid commit message', $gitLastCommitMessage);
-        } else {
+        } elseif (is_string($options['commit-msg'])) {
             $this->say("Commit message is set to <comment>{$options['commit-msg']}</comment>.");
             return $options['commit-msg'];
         }
+        throw new PolymerException('Failed to get a valid commit message.');
+    }
+
+    /**
+     * Gets the name of the tag to cut.
+     *
+     * @param array<string, bool|string|null|int> $options
+     *   Options.
+     *
+     * @return string
+     *   Name.
+     *
+     * @throws \Exception
+     */
+    protected function getTagName(array $options): string
+    {
+        if ($options['tag'] && is_string($options['tag'])) {
+            $tag_name = $options['tag'];
+        } else {
+            $tag_name = $this->ask('Enter the tag name for the deployment artifact, e.g., 1.0.0-build');
+        }
+
+        if (empty($tag_name)) {
+            // @todo Validate tag name is valid, e.g., no spaces or special characters.
+            throw new PolymerException("You must enter a valid tag name.");
+        } else {
+            $this->say("Tag is set to <comment>$tag_name</comment>.");
+        }
+
+        return $tag_name;
     }
 }
