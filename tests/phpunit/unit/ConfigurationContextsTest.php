1eb10cbe24be3148528ed2beea7815b383a8cca7
DDEV User <nobody@example.com>
2025-04-06 17:56:29 -0400

    Fix names again.

--- tests/phpunit/unit/ConfigurationContextsTest.php
+++ tests/phpunit/unit/ConfigurationContextsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DigitalPolygon\PolymerTests\phpunit\integration;
+namespace DigitalPolygon\PolymerTests\phpunit\unit;
 
 use PHPUnit\Framework\TestCase;
 use Consolidation\Config\Config;
